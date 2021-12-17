ec7c35023dcbf2f103601bcee3869872a398e60e
Gawsan <gawsan98@gmail.com>
2021-12-18 00:21:14 +0530

    Authentication and new page added

--- app/src/main/java/com/gawsan/whatsapp_clone/MainActivity.java
+++ app/src/main/java/com/gawsan/whatsapp_clone/MainActivity.java
@@ -1,17 +1,40 @@
 package com.gawsan.whatsapp_clone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
     private EditText upno,ucode;
     private Button uverify;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        @Override
+        public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+            signInWithPhoneAuthCredetial(phoneAuthCredential);
+        }
+
+        @Override
+        public void onVerificationFailed(@NonNull FirebaseException e) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +46,42 @@ public class MainActivity extends AppCompatActivity {
         ucode = findViewById(R.id.code);
         uverify=findViewById(R.id.verify);
 
+
         uverify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startPhoneNumberVerification();
+                startVerification();
 
             }
         });
 
     }
 
-    private void startPhoneNumberVerification(){
+    private void signInWithPhoneAuthCredetial(PhoneAuthCredential phoneAuthCredential){
+        FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                    UseIsLoggedIN();
+
+            }
+        });
+    }
+
+    private void UseIsLoggedIN() {
+        FirebaseUser user =FirebaseAuth.getInstance().getCurrentUser();
+
+        if(user!=null){
+            startActivity(new Intent(getApplicationContext(),MainPage.class));
+            finish();
+            return;
+        }
+
+    }
+
+    private void startVerification(){
+        //1 st  one phone number next one timer
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(upno.getText().toString(),60, TimeUnit.SECONDS, this,mCallbacks);
 
     }
 }
---
+++ app/src/main/java/com/gawsan/whatsapp_clone/MainPage.java
@@ -0,0 +1,16 @@
+package com.gawsan.whatsapp_clone;
+
+import static com.gawsan.whatsapp_clone.R.layout.*;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_main_page);
+    }
+}
