cd896edf0b1b6e10389671fee6417765fece587f
Gawsan <gawsan98@gmail.com>
2021-12-17 23:39:05 +0530

    phone number and verify code

--- app/src/main/java/com/gawsan/whatsapp_clone/MainActivity.java
+++ app/src/main/java/com/gawsan/whatsapp_clone/MainActivity.java
@@ -3,12 +3,37 @@ package com.gawsan.whatsapp_clone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.FirebaseApp;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText upno,ucode;
+    private Button uverify;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
+
+        upno =findViewById(R.id.pno);
+        ucode = findViewById(R.id.code);
+        uverify=findViewById(R.id.verify);
+
+        uverify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startPhoneNumberVerification();
+
+            }
+        });
+
+    }
+
+    private void startPhoneNumberVerification(){
+
     }
 }
