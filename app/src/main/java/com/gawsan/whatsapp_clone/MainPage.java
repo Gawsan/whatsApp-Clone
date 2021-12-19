e572a06fbcbf03238f0079ee503b0c2c142a3ea2
Gawsan <gawsan98@gmail.com>
2021-12-19 12:12:50 +0530

    Logout Page Added

--- app/src/main/java/com/gawsan/whatsapp_clone/MainPage.java
+++ app/src/main/java/com/gawsan/whatsapp_clone/MainPage.java
@@ -2,9 +2,15 @@ package com.gawsan.whatsapp_clone;
 
 import static com.gawsan.whatsapp_clone.R.layout.*;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainPage extends AppCompatActivity {
 
@@ -12,5 +18,20 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(activity_main_page);
+
+        Button mlogout =findViewById(R.id.logout);
+
+        mlogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                finish();
+                return;
+            }
+        });
+
     }
 }
